7c789e1c0a9b309fb41de94fa93cd849d5329e7d
34678 <3383789730@qq.com>
2018-05-17 02:30:53 +0800

    完成丑陋版本

--- src/components/score/myarea.vue
+++ src/components/score/myarea.vue
@@ -16,60 +16,45 @@ import selector from '../base/select.vue'
 export default {
   data(){
     return {
-      option : {
-          title : {
-              text: '对电影类型的喜好',
-              subtext: '豆瓣大数据'
-          },
-          tooltip : {
-              trigger: 'axis'
-          },
-          legend: {
-              data:['电影数量']
-          },
-          toolbox: {
-              show : true,
-              feature : {
-                  dataView : {show: true, readOnly: false},
-                  magicType : {show: true, type: ['line', 'bar']},
-                  restore : {show: true},
-                  saveAsImage : {show: true}
-              }
-          },
-          calculable : true,
-          xAxis : [
-            {
-                type : 'value',
-            }
-          ],
-          yAxis : [
-             {
-                  type : 'category',
-                  data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
-                    axisLabel:{  
-                        interval: 0  
-                    },  
-              }
-          ],
-          series : [
-              {
-                  name:'电影数量',
-                  type:'bar',
-                  data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
-                  markPoint : {
-                      data : [
-                          {type : 'max', name: '最大值'},
-                          {type : 'min', name: '最小值'}
-                      ]
-                  },
-                  markLine : {
-                      data : [
-                          {type : 'average', name: '平均值'}
-                      ]
-                  }
-              }
-          ]
-      },
+        option : {
+            title : {
+                text: 'area分析',
+                subtext: '豆瓣大数据',
+                x:'center'
+            },
+            tooltip : {
+                trigger: 'item',
+                
+                
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            legend: {
+                type: 'scroll',
+                orient: 'vertical',
+                right: 10,
+                top: 20,
+                bottom: 20,
+                data: {},
+
+                selected: {}
+            },
+            series : [
+                {
+                    name: '地区',
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['40%', '50%'],
+                    data: {},
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        },
       current:'地区'
     }},
   components: {
@@ -114,50 +99,72 @@ export default {
         }
         })
         .then(function (response) {
-        var arr = response.data.data;
-        /* console.log(arr); */
-        var x = [];
-        var y = [];
-        for(var n in arr){
-            y.push(arr[n].produce_place);
-            x.push(arr[n].count);
-        }
-        vm.option.yAxis[0].data = y;
-        vm.option.series[0].data = x;
+            var arr = response.data.data;
+            /* console.log(arr); */
+            var data1 = [];
+            var data2 = {};
+            var data3 = [];
+            var count = 0;
+            for(var n in arr){
+                data1.push({
+                    'name':arr[n].produce_place,
+                    'value' : arr[n].count
+                });
+                if(count < 10){
+                    data2[arr[n].produce_place] = true;
+                }else{
+                    data2[arr[n].produce_place] = false;
+                    
+                }
+                count++;
+                data3.push(arr[n].produce_place);
+            }
         //重新渲染echrts
-        var myChart = vm.$echarts.init(
-        document.getElementById("chart")
-         );
-         myChart.setOption(vm.option);
-/*         console.log('2' ,vm.option.yAxis[0].data)
-        console.log('2' ,vm.option.series[0].data) */
-        })
-        .catch(function (response) {
-        //错误处理 比如出现一个蒙层显示网络错误
-        console.log(response);
+            var myChart = vm.$echarts.init(
+            document.getElementById("chart")
+            );
+            vm.option.series[0].data = data1;
+            vm.option.legend. selected = data2;
+            vm.option.legend.data = data3;
+            myChart.setOption(vm.option);
+        }).catch(function (response) {
+            //错误处理 比如出现一个蒙层显示网络错误
+            console.log(response);
         });
     },
     reqType(){
-         var vm = this;
+          var vm = this;
         this.$http.get('http://193.112.138.190:666/getTypeVSRank', {
         params: {
         }
         })
         .then(function (response) {
             var arr = response.data.data;
-            /* console.log(arr); */
-            var x = [];
-            var y = [];
+            var data1 = [];
+            var data2 = {};
+            var data3 = [];
+            var count = 0;           
             for(var n in arr){
-                y.push(arr[n].movie_type);
-                x.push(arr[n].count);
+                data1.push({
+                    'name':arr[n].movie_type,
+                    'value' : arr[n].count
+                });
+                if(count < 10){
+                    data2[arr[n].movie_type] = true;
+                }else{
+                    data2[arr[n].movie_type] = false;
+                    
+                }
+                count++;
+                data3.push(arr[n].movie_type);
             }
-            vm.option.yAxis[0].data = y;
-            vm.option.series[0].data = x;
             //重新渲染echrts
             var myChart = vm.$echarts.init(
             document.getElementById("chart")
             );
+            vm.option.series[0].data = data1;
+            vm.option.legend. selected = data2;
+            vm.option.legend.data = data3;
             myChart.setOption(vm.option);
 
         //console.log(arr);
@@ -165,7 +172,7 @@ export default {
         .catch(function (response) {
         //错误处理 比如出现一个蒙层显示网络错误
         console.log(response);
-        });
+        }); 
     },
   }
 }
--- src/components/score/mytype.vue
+++ src/components/score/mytype.vue
@@ -19,32 +19,201 @@ export default {
       option : {
           title: {
               text: '电影类型/地区与数量关系',
-              subtext: '纯属虚构'
+              subtext: '豆瓣大数据'
           },
           xAxis: {
               type: 'category',
-              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+              name:'类型',
+              axisLabel:{  
+                interval: 0  
+              },  
+            
           },
-          yAxis: {
-              type: 'value'
+          yAxis: 
+          {
+              type: 'value',
+              name : '数量'
           },
           series: [{
               data: [820, 932, 901, 934, 1290, 1330, 1320],
               type: 'line'
           }]
-      }
+      },
+      current:'类型',
+      tmp : {},
+      tmp2 :{}
     }
   },
   mounted() {
+    this.option.title.text = this.current + "近年来的增长变化";
     this.__init();
+    this.reqtype();
   },
   methods: {
     __init() {
       var myChart = this.$echarts.init(
-        document.getElementById("chart")
+      document.getElementById("chart")
       );
       myChart.setOption(this.option);
-    }
+      //监听select的change事件
+      /*  console.log(this.$('type').change) */
+      var vm = this;
+      this.$('#type').change(function(){ 
+      var obj = document.getElementsByTagName("select")[0];
+      vm.current = obj.options[obj.selectedIndex].value;
+      //拿到更新的数据之后重新渲染
+      vm.option.title.text = vm.current + "近年来的增长变化";
+      var myChart = vm.$echarts.init(
+      document.getElementById("chart")
+      );
+      myChart.setOption(vm.option);
+      //请求当前类型的数据
+      if(vm.current == '地区'){
+      vm.reqArea();
+      }else{
+      vm.reqtype();
+      }
+      });
+    },
+    reqArea(){
+        var vm = this;
+      // 如果有之前的select就删除
+      if(vm.$('#type2').length !== 0 ){
+        vm.$('#type2').remove();
+      }
+        this.$http.get('http://193.112.138.190:666/getRegionYearAnalysic')
+        .then(function (response) {
+            var arr = response.data.data;
+            // 存储所有地区变化的临时数据
+            vm.tmp = arr;
+            var res = [];
+            for(var n in arr){
+              res.push(arr[n].region);
+            }
+            // 根据地区名称下来框
+            if(vm.$('#area').length == 0 ){
+              var text = '<select id="area" style="    float: right;margin-right: 173px;height: 36px;">'
+              for(var n in res){
+                text += "<option>"+ res[n] + "</option>";
+              }
+              text += "</select>";
+              vm.$('#type').after(vm.$(text));
+            }
+            var tmpArr = vm.tmp[0].result.split(';');
+            var arr1=[],
+                arr2=[];
+            for(var n in tmpArr){
+              var newTable = tmpArr[n].split('-');
+              arr1.push(newTable[0]);
+              arr2.push(newTable[1]);
+            }
+            vm.$('#area').change(function(){
+              var obj = document.getElementsByTagName("select")[1];
+              var area = obj.options[obj.selectedIndex].value;
+              // 从temp里面找到相关数据并且显示
+              for(var n in vm.tmp){
+                if(vm.tmp[n].region == area){
+                    var tmpArr = vm.tmp[n].result.split(';');
+                    var arr1=[],
+                        arr2=[];
+                    for(var n in tmpArr){
+                      var newTable = tmpArr[n].split('-');
+                      arr1.push(newTable[0]);
+                      arr2.push(newTable[1]);
+                    }
+                    //重新渲染echrts
+                    var myChart = vm.$echarts.init(
+                    document.getElementById("chart")
+                    );
+                    vm.option.xAxis.data = arr1;
+                    vm.option.series[0].data = arr2;
+                    myChart.setOption(vm.option);  
+                }
+              }
+
+            })
+            //重新渲染echrts
+            var myChart = vm.$echarts.init(
+            document.getElementById("chart")
+            );
+            vm.option.xAxis.data = arr1;
+            vm.option.series[0].data = arr2;
+            myChart.setOption(vm.option);
+        }).catch(function (response) {
+            //错误处理 比如出现一个蒙层显示网络错误
+            console.log(response);
+        });
+    },
+    reqtype(){
+      var vm = this;
+      // 如果有之前的select就删除
+      if(vm.$('#area').length !== 0 ){
+        vm.$('#area').remove();
+      }
+        this.$http.get('http://193.112.138.190:666/getTypeYearAnalysic')
+        .then(function (response) {
+            var arr = response.data.data;
+            // 存储所有地区变化的临时数据
+            vm.tmp2 = arr;
+            var res = [];
+            for(var n in arr){
+              res.push(arr[n].type);
+            }
+            // 根据地区名称下来框
+            if(vm.$('#type2').length == 0 ){
+              var text = '<select id="type2" style="    float: right;margin-right: 173px;height: 36px;">'
+              for(var n in res){
+                text += "<option>"+ res[n] + "</option>";
+              }
+              text += "</select>";
+              vm.$('#type').after(vm.$(text));
+            }
+            var tmpArr = vm.tmp2[0].result.split(';');
+            var arr1=[],
+                arr2=[];
+            for(var n in tmpArr){
+              var newTable = tmpArr[n].split('-');
+              arr1.push(newTable[0]);
+              arr2.push(newTable[1]);
+            }
+            vm.$('#type2').change(function(){
+              var obj = document.getElementsByTagName("select")[1];
+              var type = obj.options[obj.selectedIndex].value;
+              // 从temp里面找到相关数据并且显示
+              for(var n in vm.tmp2){
+                if(vm.tmp2[n].type == type){
+                    var tmpArr = vm.tmp2[n].result.split(';');
+                    var arr1=[],
+                        arr2=[];
+                    for(var n in tmpArr){
+                      var newTable = tmpArr[n].split('-');
+                      arr1.push(newTable[0]);
+                      arr2.push(newTable[1]);
+                    }
+                    //重新渲染echrts
+                    var myChart = vm.$echarts.init(
+                    document.getElementById("chart")
+                    );
+                    vm.option.xAxis.data = arr1;
+                    vm.option.series[0].data = arr2;
+                    myChart.setOption(vm.option);  
+                }
+              }
+
+            })
+            //重新渲染echrts
+            var myChart = vm.$echarts.init(
+            document.getElementById("chart")
+            );
+            vm.option.xAxis.data = arr1;
+            vm.option.series[0].data = arr2;
+            myChart.setOption(vm.option);
+        }).catch(function (response) {
+            //错误处理 比如出现一个蒙层显示网络错误
+            console.log(response);
+        });
+    },
   },
   components: {
       anotherTab,
@@ -56,7 +225,7 @@ export default {
 <style scoped>
 #chart{
     height: 607px!important;
-    width: 800px;
+    width: 1300px;
     margin: 0 auto;
 }
 select{
